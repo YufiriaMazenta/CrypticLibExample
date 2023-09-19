f3557e6fe494b313db58d6ab2094a505a3efd240
ChiyodaXiaoYi <linjiayi357159@gmail.com>
2023-09-19 09:25:37 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import java.text.SimpleDateFormat
+
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow").version("7.1.2")
+}
+
+repositories {
+    mavenLocal()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+    maven("https://repo.maven.apache.org/maven2/")
+    mavenCentral()
+}
+
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    implementation("com.github.YufiriaMazenta:CrypticLib:1.0.5")
+}
+
+group = "com.example"
+version = "1.0.0"
+var mainClass = "${rootProject.group}.${rootProject.name.lowercase()}.Example"
+var pluginVersion: String = version.toString() + "-" + SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis())
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks {
+    val props = HashMap<String, String>()
+    props["version"] = pluginVersion
+    props["main"] = mainClass
+    props["name"] = rootProject.name
+    processResources {
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    shadowJar {
+        relocate("crypticlib", "${rootProject.group}.crypticlib")
+    }
+}
---
+++ src/main/java/com/example/crypticlibexample/Example.java
@@ -0,0 +1,17 @@
+package com.example.crypticlibexample;
+
+import crypticlib.BukkitPlugin;
+
+public class Example extends BukkitPlugin {
+
+    @Override
+    public void enable() {
+        //TODO
+    }
+
+    @Override
+    public void disable() {
+        //TODO
+    }
+
+}
