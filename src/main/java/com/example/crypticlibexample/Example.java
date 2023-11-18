1f5f2f1a26496d83ff30cd290610b6591adabd0c
YufiriaMazenta <2199098065@qq.com>
2023-11-18 21:56:01 +0800

    更新CrypticLib版本0.1.12

--- src/main/java/com/example/crypticlibexample/Example.java
+++ src/main/java/com/example/crypticlibexample/Example.java
@@ -2,12 +2,36 @@ package com.example.crypticlibexample;
 
 import crypticlib.BukkitPlugin;
 import crypticlib.CrypticLib;
+import crypticlib.command.CommandInfo;
+import crypticlib.command.RootCmdExecutor;
+import crypticlib.util.MsgUtil;
+
+import static crypticlib.command.CommandManager.subcommand;
+import static crypticlib.command.CommandManager.rootCommand;
 
 public class Example extends BukkitPlugin {
 
     @Override
     public void enable() {
         saveDefaultConfig();
+        CrypticLib.commandManager().register(
+                this,
+                new CommandInfo("example", "example.command", new String[]{"exa"}, "Example command", "/example or /exa"),
+                (RootCmdExecutor) rootCommand()
+                        .setExecutor((sender, args) -> {
+                            MsgUtil.sendMsg(sender, "Hello, CrypticLib!");
+                            return true;
+                        })
+                        .regSub(subcommand("test")
+                                        .setExecutor((sender, args) -> {
+                                            MsgUtil.sendMsg(sender, "CrypticLib Test!");
+                                            return true;
+                                        }))
+                        .regSub("test2", (sender, args) -> {
+                            MsgUtil.sendMsg(sender, "CrypticLib Test2!");
+                            return true;
+                        })
+        );
     }
 
     @Override
