52ea3bf570eeec3a65dfcd4747f305d736e097f6
ChiyodaXiaoYi <linjiayi357159@gmail.com>
2023-11-19 10:03:52 +0800

    更新CrypticLib版本0.2.0

--- src/main/java/com/example/crypticlibexample/Example.java
+++ src/main/java/com/example/crypticlibexample/Example.java
@@ -1,37 +1,39 @@
 package com.example.crypticlibexample;
 
 import crypticlib.BukkitPlugin;
-import crypticlib.CrypticLib;
-import crypticlib.command.CommandInfo;
-import crypticlib.command.RootCmdExecutor;
+import crypticlib.command.api.CommandInfo;
+import crypticlib.command.impl.RootCmdExecutor;
 import crypticlib.util.MsgUtil;
 
 import static crypticlib.command.CommandManager.subcommand;
-import static crypticlib.command.CommandManager.rootCommand;
 
 public class Example extends BukkitPlugin {
 
     @Override
     public void enable() {
         saveDefaultConfig();
-        CrypticLib.commandManager().register(
+        new RootCmdExecutor()
+            .setExecutor((sender, args) -> {
+                MsgUtil.sendMsg(sender, "Hello, CrypticLib!");
+                return true;
+            })
+            .regSub(subcommand("test")
+                .setExecutor((sender, args) -> {
+                    MsgUtil.sendMsg(sender, "CrypticLib Test!");
+                    return true;
+                })
+                .regSub("test", (sender, args) -> {
+                    MsgUtil.sendMsg(sender, "CrypticLib Test Test!");
+                    return true;
+                }))
+            .regSub("test2", (sender, args) -> {
+                MsgUtil.sendMsg(sender, "CrypticLib Test2!");
+                return true;
+            })
+            .register(
                 this,
-                new CommandInfo("example", "example.command", new String[]{"exa"}, "Example command", "/example or /exa"),
-                (RootCmdExecutor) rootCommand()
-                        .setExecutor((sender, args) -> {
-                            MsgUtil.sendMsg(sender, "Hello, CrypticLib!");
-                            return true;
-                        })
-                        .regSub(subcommand("test")
-                                        .setExecutor((sender, args) -> {
-                                            MsgUtil.sendMsg(sender, "CrypticLib Test!");
-                                            return true;
-                                        }))
-                        .regSub("test2", (sender, args) -> {
-                            MsgUtil.sendMsg(sender, "CrypticLib Test2!");
-                            return true;
-                        })
-        );
+                new CommandInfo("example", "example.command", new String[]{"exa"}, "Example command", "/example or /exa")
+            );
     }
 
     @Override
