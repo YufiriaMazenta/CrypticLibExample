572dc4b554751d3785747f67c42fbb0780635269
YufiriaMazenta <2199098065@qq.com>
2023-12-16 12:38:40 +0800

    更新CrypticLib版本0.8.3

--- src/main/java/com/example/crypticlibexample/Configs.java
+++ src/main/java/com/example/crypticlibexample/Configs.java
@@ -3,18 +3,9 @@ package com.example.crypticlibexample;
 import crypticlib.chat.LangConfigEntry;
 import crypticlib.chat.LangConfigHandler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @LangConfigHandler(langFileFolder = "lang")
 public class Configs {
 
-    public static final LangConfigEntry test = new LangConfigEntry("test", "config test", () -> {
-        Map<String, String> langMap = new HashMap<>();
-        langMap.put("zh_cn", "你好");
-        langMap.put("en_us", "Hello");
-        langMap.put("zh_tw", "你好");
-        return langMap;
-    });
+    public static final LangConfigEntry test = new LangConfigEntry("test", "config test");
 
 }
