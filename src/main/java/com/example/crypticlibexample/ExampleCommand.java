c3b2242c4404ae989bfc7de5d62e201f3d5e264c
YufiriaMazenta <2199098065@qq.com>
2024-07-24 20:18:47 +0800

    更新CrypticLib版本

--- src/main/java/com/example/crypticlibexample/ExampleCommand.java
+++ src/main/java/com/example/crypticlibexample/ExampleCommand.java
@@ -2,7 +2,7 @@ package com.example.crypticlibexample;
 
 import crypticlib.chat.BukkitMsgSender;
 import crypticlib.command.BukkitCommand;
-import crypticlib.command.BukkitSubCommand;
+import crypticlib.command.BukkitSubcommand;
 import crypticlib.command.CommandInfo;
 import crypticlib.command.annotation.Command;
 import crypticlib.command.annotation.Subcommand;
@@ -10,29 +10,46 @@ import crypticlib.perm.PermInfo;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Command
 public class ExampleCommand extends BukkitCommand {
 
     public ExampleCommand() {
-        super(new CommandInfo("test", new PermInfo("test.test"), new String[] {"tt"}));
+        super(new CommandInfo(
+            "test",
+            new PermInfo("test.test"),
+            Arrays.asList("tt"),
+            "测试命令",
+            "用法: &r/test &7<...>"
+            )
+        );
     }
 
     @Subcommand
-    BukkitSubCommand reload = new BukkitSubCommand("reload", new PermInfo("test.test.reload")) {
+    BukkitSubcommand reload = new BukkitSubcommand(
+        new CommandInfo(
+            "reload",
+            new PermInfo("test.test.reload"),
+            Collections.emptyList(),
+            "&7重载插件"
+        )
+    ) {
         @Override
         public void execute(@NotNull CommandSender sender, @NotNull List<String> args) {
             Example.INSTANCE.reloadPlugin();
             BukkitMsgSender.INSTANCE.sendMsg(sender, ExampleConfig.test.value());
         }
-        @Subcommand
-        BukkitSubCommand test = new BukkitSubCommand("test2") {
-            @Override
-            public void execute(@NotNull CommandSender sender, @NotNull List<String> args) {
-                BukkitMsgSender.INSTANCE.sendMsg(sender, ExampleConfig.test2.value().toString());
-            }
-        };
+    };
+
+    @Subcommand
+    BukkitSubcommand test = new BukkitSubcommand("test2") {
+        @Override
+        public void execute(@NotNull CommandSender sender, @NotNull List<String> args) {
+            BukkitMsgSender.INSTANCE.sendMsg(sender, ExampleConfig.test2.value().toString());
+        }
     };
 
 
