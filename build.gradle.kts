c128589090a1db1712e444ee181d40360d360e55
Yufiria <2199098065@qq.com>
2026-07-25 17:08:46 +0800

    更新CrypticLib版本至1.19.3.2，修改为kotlin项目

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,11 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("io.github.goooler.shadow").version("8.1.8")
+    kotlin("jvm") version "2.4.10"
 }
 
 rootProject.group = rootProject.findProperty("group").toString()
 rootProject.version = rootProject.findProperty("version")!!
-java.sourceCompatibility = JavaVersion.VERSION_21
-java.targetCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenLocal()
@@ -21,6 +20,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly(kotlin("stdlib"))
     implementation("com.crypticlib:bukkit:${rootProject.findProperty("crypticlibVersion")}")
 }
 
@@ -40,6 +40,9 @@ tasks {
     }
     shadowJar {
         relocate("crypticlib", rootProject.findProperty("crypticlibRelocatePackage").toString())
+        relocate("kotlin", "kotlin2410")
+        relocate("org.intellij.lang.annotations", "example.libs.intellij.lang.annotations")
+        relocate("org.jetbrains.annotations", "example.libs.jetbrains.annotations")
         archiveFileName.set("${rootProject.name}-${rootProject.version}.jar")
     }
     val props = HashMap<String, String>()
@@ -51,4 +54,8 @@ tasks {
             expand(props)
         }
     }
+}
+
+kotlin {
+    jvmToolchain(21)
 }
---
+++ src/main/kotlin/example/Example.kt
@@ -0,0 +1,43 @@
+package example
+
+import crypticlib.BukkitPlugin
+import crypticlib.CrypticLibBukkit
+import crypticlib.MinecraftVersion
+import crypticlib.dependency.Dependency
+import crypticlib.dependency.DependencyLoader
+import crypticlib.util.IOHelper
+import org.bukkit.event.Listener
+
+class Example : BukkitPlugin(), Listener {
+
+    init {
+        try {
+            DependencyLoader.INSTANCE.loadDependency(
+                Dependency
+                    .builder("org.jetbrains.kot#lin", "kot#lin-stdlib", "2.4.10")
+                    .test("!kot#lin2410%Kot#linVersion")
+                    .relocate("kot#lin", "kot#lin2410")
+                    .relocate("org%intellij%lang%annotations", "example%libs%intellij%lang%annotations")
+                    .relocate("org%jetbrains%annotations", "example%libs%jetbrains%annotations")
+                    .build()
+            )
+        } catch (e: Throwable) {
+            throw RuntimeException(e)
+        }
+        INSTANCE = this
+    }
+
+    override fun whenEnable() {
+        IOHelper.info("Server Type: " + CrypticLibBukkit.serverAdapter().type().name)
+        IOHelper.info("Server Version: " + MinecraftVersion.CURRENT.name)
+    }
+
+    override fun whenDisable() {
+        IOHelper.info("Plugin disabled.")
+    }
+
+    companion object {
+        lateinit var INSTANCE: Example
+    }
+
+}
