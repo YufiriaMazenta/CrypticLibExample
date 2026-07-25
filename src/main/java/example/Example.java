80146e6200eb9688465431ab421a4ab07e736643
Yufiria <2199098065@qq.com>
2026-07-25 18:11:23 +0800

    插件主类修改为java代码,添加kotlin的监听器示例

---
+++ src/main/java/example/Example.java
@@ -0,0 +1,42 @@
+package example;
+
+import crypticlib.BukkitPlugin;
+import crypticlib.CrypticLibBukkit;
+import crypticlib.MinecraftVersion;
+import crypticlib.dependency.Dependency;
+import crypticlib.dependency.DependencyLoader;
+import crypticlib.util.IOHelper;
+import org.bukkit.event.Listener;
+
+public final class Example extends BukkitPlugin implements Listener {
+
+    public Example() {
+        try {
+            DependencyLoader.INSTANCE.loadDependency(
+                Dependency
+                    .builder("org.jetbrains.kot#lin", "kot#lin-stdlib", "2.4.10")
+                    .test("!kotlin.KotlinVersion")
+                    .repository(Dependency.REPOSITORY_MAVEN_CENTRAL)
+                    .repository(Dependency.REPOSITORY_MAVEN_CENTRAL_MIRROR_ALI)
+                    .relocate("kot#lin", "kot#lin2410")
+                    .relocate("org%intellij%lang%annotations", "example%libs%intellij%lang%annotations")
+                    .relocate("org%jetbrains%annotations", "example%libs%jetbrains%annotations")
+                    .build()
+            );
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void whenEnable() {
+        IOHelper.info("Server Type: " + CrypticLibBukkit.serverAdapter().type().name());
+        IOHelper.info("Server Version: " + MinecraftVersion.CURRENT.name());
+    }
+
+    @Override
+    public void whenDisable() {
+        IOHelper.info("Plugin disabled.");
+    }
+
+}
