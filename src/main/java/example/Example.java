861381d1b57d9a6019fee4e40490a3b4614aa348
Yufiria <2199098065@qq.com>
2026-07-26 10:47:44 +0800

    更新CrypticLib版本1.19.5.0

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -5,6 +5,7 @@ import crypticlib.CrypticLibBukkit;
 import crypticlib.MinecraftVersion;
 import crypticlib.dependency.Dependency;
 import crypticlib.dependency.DependencyLoader;
+import crypticlib.dependency.Repository;
 import crypticlib.util.IOHelper;
 import org.bukkit.event.Listener;
 
@@ -16,8 +17,8 @@ public final class Example extends BukkitPlugin implements Listener {
                 Dependency
                     .builder("org.jetbrains.kot#lin", "kot#lin-stdlib", "2.4.10")
                     .test("!kot#lin2420%Kot#linVersion")
-                    .repository(Dependency.REPOSITORY_MAVEN_CENTRAL)
-                    .repository(Dependency.REPOSITORY_MAVEN_CENTRAL_MIRROR_ALI)
+                    .repository(Repository.MAVEN_CENTRAL_MIRROR_ALI)
+                    .repository(Repository.MAVEN_CENTRAL)
                     .relocate("kot#lin", "kot#lin2420")
                     .relocate("org%intellij%lang%annotations", "example%libs%intellij%lang%annotations")
                     .relocate("org%jetbrains%annotations", "example%libs%jetbrains%annotations")
